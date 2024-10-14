0b0a5a8af456cca726678a860e12f5cb524523d0
FSB <93573184+FriendlyStepBro@users.noreply.github.com>
2024-10-14 21:58:41 +1000

    Fixed bad header file location issue in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ from setuptools import setup, Extension
 # Detect the platform
 if platform.system() == 'Windows':
     source_file = 'win_Resize.c'
+    include_dirs = []  # Windows doesn't need special include paths for Tk
 else:
     source_file = 'unix_Resize.c'
+    include_dirs = ['/usr/include/tcl', '/usr/include/tk']  # Add correct include paths for Tk
 
 module = Extension(
     'resize_event',
-    sources=[source_file],  # Use the correct source file based on OS
+    sources=[source_file],
+    include_dirs=include_dirs,  # Add include directories for Tk
     libraries=['tk', 'tcl']
 )
 
